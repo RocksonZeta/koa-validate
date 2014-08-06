673db91a788d919fc1bc46af9ecd01f3c7e84f38
uzoice <uzoice@163.com>
2014-08-06 23:05:58 +0800

    first version

--- README.md
+++ README.md
@@ -2,3 +2,28 @@ koa-validate
 ============
 
 validate koa request params and format request params 
+[![build status][travis-image]][travis-url]
+[![Test coverage][coveralls-image]][coveralls-url]
+
+##installcation
+```
+$ npm install koa-validate
+```
+
+
+## basic usage:
+```javascript
+	app.use(require('koa-body')());
+	app.use(require('koa-validate')());
+	app.use(require('koa-router')(app));
+	app.post('/signin',function*(){
+		this.checkBody('name').optional().len(3,20);
+		this.checkBody('email').isEmail();
+		this.checkBody('password').notEmpty().len(3,20);
+		this.checkBody('nick').optional().empty().len(3,20);
+		if(this.errors){
+			return this.body = this.errors;
+		}
+		this.body= 'ok';
+	});
+```
--- test/test_validate.js
+++ test/test_validate.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var koa = require('koa'),
+ http     = require('http'),
 request = require('supertest');
+
 
 function create_app(){
 	var app = koa();
@@ -11,35 +13,27 @@ function create_app(){
 	return app;
 }
 
-// describe('koa-validate' , function(){
-// 	it("optional should be to ok" , function(done){
+describe('koa-validate' , function(){
+	it("these validates should be to ok" , function(done){
+		var app = create_app();
+		app.post('/signin',function*(){
+			this.checkBody('name').optional().len(3,20);
+			this.checkBody('email').isEmail();
+			this.checkBody('password').notEmpty().len(3,20);
+			this.checkBody('nick').optional().empty().len(3,20);
+			if(this.errors){
+				return this.body = this.errors;
+			}
+			this.body= 'ok';
+		});
+		var req = request(app.listen());
 
-// 		var app = create_app();
-// 		app.post('/',function*(){
-// 			this.checkBody('name').optional();
-// 			if(this.errors){
-// 				return this.throw(500); 
-// 			}
-// 		});
-// 		request(app.listen())
-// 		.post('/')
-// 		.send({name:"jim"})
-// 		.expect(200)
-// 		.end(done);
+		req.post('/signin')
+		.send({name:"jim",email:"jim@gmail.com",password:"tom",nick:"0"})
+		.expect(200)
+		.expect('ok' ,done);
 		
-// 	});
-// });
-
-
-var app = create_app();
-	app.post('/',function*(){
-		return this.body = "hahah";
-		this.checkBody('name').optional();
-		if(this.errors){
-			return this.throw(500); 
-		}
 	});
-	app.listen();
-
+});
 
 
